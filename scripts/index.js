68c1dcb89688331ed4117ab1d7fccf9aa4c6e2af
Eytor Lima <eytorlima@outlook.com>
2023-11-08 17:00:03 -0300

    Correções pontuais

--- scripts/index.js
+++ scripts/index.js
@@ -5,19 +5,19 @@ class MobileNavBar {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+        this.activeNav = "active-nav";
+        this.activeMenu = "active-menu";
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick() {
-        this.navList.classList.toggle(this.activeClass);
+        this.navList.classList.toggle(this.activeNav);
+        this.mobileMenu.classList.toggle(this.activeMenu);
     }
     addClickEvent() {
         this.mobileMenu.addEventListener('click', this.handleClick);
 
         this.navLinks.forEach(link => {
-            link.addEventListener('click', () => {
-                this.handleClick();
-            });
+            link.addEventListener('click', this.handleClick);
         });
     }
     init() {
